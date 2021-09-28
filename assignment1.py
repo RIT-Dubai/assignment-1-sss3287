5fb359218f9fe0301e4bd0d7b58a67c31a0c20a0
sss3287 <sss3287@rit.edu>
2021-09-28 22:07:28 +0400

    rectangle will fit function works and all tests passed
    draw shape works correctly with all tests passed

--- assignment1.py
+++ assignment1.py
@@ -5,27 +5,51 @@ Y_MAX = 400
 Y_MIN = -Y_MAX
 
 #Below checks if rectangle will fit in python window
-def rectangle_will_fit(x, y, length, height):
-    if X_MIN <= x <= X_MAX and Y_MIN <= y <= Y_MAX and X_MIN <= x + length <= X_MAX and Y_MIN <= y - height:
+
+
+def rectangle_will_fit(x, y, length1, height1):
+    if X_MIN <= x <= X_MAX and Y_MIN <= y <= Y_MAX and X_MIN <= x + length1 <= X_MAX and Y_MIN <= y - height1:
         print ('True')
     else:
         print('False')
 
 #Below draws a rectangle of the specified shape,color,x,y,length and height.
+
+
 def draw_shape(shape, color_code, x, y, length, height):
 
     if shape == 'r':
         turtle.begin_fill()
         turtle.fillcolor(color_code)
-    for i in range(2):
-        turtle.forward(length)
-        turtle.right(90)
-        turtle.forward(height)
-        turtle.right(90)
-    turtle.end_fill()
-    input()
+        for i in range(2):
+            turtle.forward(length)
+            turtle.left(90)
+            turtle.forward(height)
+            turtle.left(90)
+        turtle.end_fill()
+        input()
+
+    elif shape == 'c':
+        turtle.begin_fill()
+        turtle.fillcolor(color_code)
+        turtle.circle(length)
+        turtle.end_fill()
+
+    elif shape == 't':
+        turtle.begin_fill()
+        turtle.fillcolor(color_code)
+        for i in range(3):
+            turtle.forward(length)
+            turtle.left(120)
+        turtle.end_fill()
+
+    else:
+        return
+
+
 
 def main():
-    draw_shape('r', 'red', 50, 50, 300, 150)
+    draw_shape('c', 'pink', 0, 0, 250, 0)
+
 
 main()
--- test_assigment1.py
+++ test_assigment1.py
@@ -2,21 +2,20 @@ import turtle
 import assignment1
 
 def test_rectangle_will_fit():
-    assignment1.rectangle_will_fit(-100,100,50,50)
+    assignment1.rectangle_will_fit(-100, 100, 50, 50)
 
     x = -100
-    assert (x==-100)
+    assert (x == -100)
 
     y = 100
-    assert (y==100)
+    assert (y == 100)
 
-    length = 50
-    assert (length==50)
+    length1 = 50
+    assert (length1 == 50)
 
-    height = 50
-    assert (height==50)
+    height1 = 50
+    assert (height1 == 50)
 
-    result = assignment1.rectangle_will_fit(x, y ,length,height)
 
 def test_draw_shape():
     assignment1.draw_shape('r', 'red', 50, 50, 300, 150)
@@ -40,5 +39,47 @@ def test_draw_shape():
     assert (height==150)
 
 
+def test_draw_shape():
+    assignment1.draw_shape('c', 'pink', 0, 0, 150, 0)
+
+    shape = 'c'
+    assert(shape=='c')
+
+    color_code = 'pink'
+    assert (color_code=='pink')
+
+    x = 0
+    assert (x==0)
+
+    y = 0
+    assert (y==0)
+
+    length = 150
+    assert (length==150)
+
+    height = 0
+    assert (height==0)
+
+
+def test_draw_shape():
+    assignment1.draw_shape('t', 'blue', 50, 50, 200, 0)
+
+    shape = 't'
+    assert(shape=='t')
+
+    color_code = 'blue'
+    assert (color_code=='blue')
+
+    x = 50
+    assert (x==50)
+
+    y = 50
+    assert (y==50)
+
+    length = 200
+    assert (length==200)
+
+    height = 0
+    assert (height==0)
 
 
