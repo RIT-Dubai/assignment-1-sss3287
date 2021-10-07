0efa84ff780631680c0d6d22fbfacdff4f267cea
sss3287 <sss3287@rit.edu>
2021-10-07 08:58:09 +0400

    Did too many modifications in Draw_shape and introduced get color and  introduced perimeters for it to calculate and return. Even added docstrings.
    And tested all functions,and it has passed and introduced docstrings.

--- assignment1.py
+++ assignment1.py
@@ -1,60 +1,96 @@
 import turtle
 import math
-X_MAX = 200
-X_MIN = -200
-Y_MAX = 200
-Y_MIN = -200
+X_MAX = 400
+X_MIN = -400
+Y_MAX = 400
+Y_MIN = -400
+
+"""This function draws the window"""
+
 
 def init():
     turtle.penup()
     turtle.tracer(0)
-    turtle.goto(X_MIN, Y_MAX)
+    turtle.goto(X_MIN//2, Y_MAX//2)
     turtle.pendown()
     for i in range(4):
         turtle.forward(400)
         turtle.right(90)
+"""This function checks if the rectangle_will_fit """
+
 
 def rectangle_will_fit(x, y, length, height):
+    XT = x-X_MAX//2
+    YT = -y+Y_MAX//2
     turtle.tracer(0)
     turtle.penup()
     turtle.goto(x, y)
     turtle.setheading(0)
-    if x+length > X_MAX or x < X_MIN:
+    if XT+length>X_MAX//2 or XT<X_MIN//2:
         return False
 
-    if y+height > Y_MAX or y < Y_MIN:
+    if YT+height>Y_MAX//2 or YT<Y_MIN//2:
         return False
 
-    return True
+    else:
+          return True
+
+"""This function checks if the circle_will_fit"""
 
 
 def circle_will_fit(x, y, length):
+    XT = x-X_MAX//2
+    YT = -y+Y_MAX//2
     turtle.penup()
     turtle.tracer(0)
     turtle.goto(x, y)
     turtle.setheading(0)
     i = 0
-    if x+length > X_MAX or x-length < X_MIN:
+    if XT+length>X_MAX or XT-length<X_MIN:
         return False
 
-    if y+length > Y_MAX or y-length < Y_MIN:
+    if YT+length>Y_MAX or YT-length<X_MIN:
         return False
 
-    return True
+
+    else:
+        return True
+
+"""This function checks if the triangle_will_fit"""
 
 
 def triangle_will_fit(x, y, length):
+    XT = x-X_MAX//2
+    YT = -y+Y_MAX//2
     turtle.penup()
     turtle.tracer(0)
     turtle.goto(x, y)
     turtle.setheading(0)
+    i = 0
     if x<X_MIN or x+length>X_MAX:
         return False
 
-    if y < Y_MIN or y+(length*math.sqrt(3)/2) > Y_MAX:
+    if y<Y_MIN or y+(length*math.sqrt(3)/2)>Y_MAX:
         return False
 
-    return True
+    else:
+        return True
+
+"""This function limits the colour"""
+
+
+def get_color(color_code):
+    color = ""
+    if color_code == "blue":
+        color = "blue"
+    elif color_code == "green":
+        color = "green"
+    elif color_code == "red":
+        color = "red"
+
+    return color
+"""This function draws a rectangle,circle and triangle"""
+
 
 def draw_shape(shape, color_code, x, y, length, height):
     turtle.tracer(1)
@@ -63,7 +99,7 @@ def draw_shape(shape, color_code, x, y, length, height):
     turtle.pendown()
     turtle.begin_fill()
     turtle.fillcolor(color_code)
-    if shape == 'r' and rectangle_will_fit(x, y, length, height):
+    if shape == 'r':
         turtle.begin_fill()
         turtle.fillcolor(color_code)
         for i in range(2):
@@ -72,9 +108,18 @@ def draw_shape(shape, color_code, x, y, length, height):
             turtle.forward(height)
             turtle.left(90)
         turtle.end_fill()
-        input()
+        perimeter = 0
+        if rectangle_will_fit(-100, -50, 90, 70) == True:
+            perimeter = 2*(length + height)
+            print("The Perimeter of rectangle is", perimeter)
 
-    elif shape == 'c' and circle_will_fit(x, y, length):
+        else:
+            perimeter = 2*(length + height)
+            print("The Perimeter of rectangle is", perimeter)
+
+        return perimeter
+
+    elif shape == 'c':
         turtle.tracer(1)
         turtle.penup()
         turtle.goto(x, y)
@@ -83,8 +128,16 @@ def draw_shape(shape, color_code, x, y, length, height):
         turtle.fillcolor(color_code)
         turtle.circle(length)
         turtle.end_fill()
+        perimeter=0
+        if circle_will_fit(100, 100, 50)==True:
+            perimeter = math.pi*length*2
+            print("The Perimeter of circle is", perimeter)
+        else:
+            print("The Perimeter of circle is", perimeter)
 
-    elif shape == 't' and triangle_will_fit(x, y, length):
+        return perimeter
+
+    elif shape == 't':
         turtle.tracer(1)
         turtle.penup()
         turtle.goto(x, y)
@@ -95,15 +148,27 @@ def draw_shape(shape, color_code, x, y, length, height):
             turtle.forward(length)
             turtle.left(120)
         turtle.end_fill()
+        perimeter=0
+        if triangle_will_fit(100, -100, 100)==True:
+            perimeter = length*3
+            print("The Perimeter of triangle is", perimeter)
+        else:
+            print("The Perimeter of triangle is", perimeter)
+
+        return perimeter
+
 
     else:
         return 0
+
 
 
 def main():
     init()
     draw_shape('t', 'blue', 100, -100, 100, 0)
     draw_shape('c', 'green', 100, 100, 50, 0)
-    draw_shape('r', 'red', -100, -50, 60, 50)
+    draw_shape('r', 'red', -50, -50, 90, 70)
+
+
 
 main()
--- test_assigment1.py
+++ test_assigment1.py
@@ -1,70 +1,129 @@
 import turtle
+import math
 import assignment1
+
+"""This function tests the init function"""
+
+
+def test_init():
+    assignment1.init()
+    x=round(turtle.xcor(), 0)
+    assert (x == -200)
+
+    y=round(turtle.ycor(), 0)
+    assert (y == 200)
+
+    pos=turtle.isdown()
+    assert (pos == True)
+
+"""This function tests the rectangle_will_fit function"""
+
+
+def test_rectangle_will_fit():
+    r=assignment1.rectangle_will_fit(-50, -50, 90, 70)
+    x=round(turtle.xcor(), 0)
+    assert (x == -50)
+
+    y=round(turtle.ycor(), 0)
+    assert (y == -50)
+
+    pos=turtle.isdown()
+    assert (pos == False)
+
+    assert(r==False)
+
+"""This function tests the circle_will_fit function"""
+
+
+def test_circle_will_fit():
+    c = assignment1.circle_will_fit(100, 100, 50)
+    x=round(turtle.xcor(), 0)
+    assert (x==100)
+
+    y=round(turtle.ycor(), 0)
+    assert (y==100)
+
+    pos=turtle.isdown()
+    assert (pos==False)
+
+    assert(c==True)
+
+"""This function tests the triangle_will_fit function"""
+
+
+def test_triangle_will_fit():
+    t=assignment1.triangle_will_fit(100, -100, 100)
+    x=round(turtle.xcor(), 0)
+    assert (x==100)
+
+    y=round(turtle.ycor(), 0)
+    assert (y==-100)
+
+    pos=turtle.isdown()
+    assert (pos==False)
+
+    assert(t==True)
+
+
+"""This function tests get_color function"""
+
+
+def test_get_color():
+    color = assignment1.get_color("blue")
+    assert(color == "blue")
+
+"""This function tests draw shape function for rectangle"""
 
 
 def test_draw_shape_rectangle():
-    assignment1.draw_shape('r', 'red', -100, -50, 90, 70)
+    r = assignment1.draw_shape('r', 'blue', -100, -50, 90, 70)
 
-    shape = 'r'
-    assert(shape == 'r')
+    assert(r == 2*(100+50))
 
-    color_code = 'red'
-    assert (color_code == 'red')
+    x = round(turtle.xcor(), 0)
+    assert(x == -100)
 
-    x = -100
-    assert (x == -100)
+    y = round(turtle.ycor(), 0)
+    assert(y == -50)
 
-    y = -50
-    assert (y == -50)
+    heading = turtle.heading()
+    assert(heading == 0)
 
-    length = 90
-    assert (length == 90)
+    color = turtle.fillcolor()
+    assert(color == "blue")
 
-    height = 70
-    assert (height == 70)
+"""This function tests the draw shape function for circle"""
 
 
 def test_draw_shape():
-    assignment1.draw_shape('c', 'green', 100, 100, 50, 0)
+    c = assignment1.draw_shape('c', 'green', 100, 100, 50, 0)
 
-    shape = 'c'
-    assert(shape == 'c')
+    assert(c == 2*math.pi*50)
 
-    color_code = 'green'
-    assert (color_code == 'green')
+    x = round(turtle.xcor(), 0)
+    assert(x == 100)
 
-    x = 100
-    assert (x == 100)
+    y = round(turtle.ycor(), 0)
+    assert(y == 100)
 
-    y = 100
-    assert (y == 100)
+    heading =turtle.heading()
+    assert(heading == 0)
 
-    length = 50
-    assert (length == 50)
+    color = turtle.fillcolor()
+    assert(color == "green")
 
-    height = 0
-    assert (height == 0)
+"""This function tests the draw shape function for triangle"""
 
 
 def test_draw_shape():
-    assignment1.draw_shape('t', 'blue', 100, -100, 100, 0)
+    t = assignment1.draw_shape('t', 'blue', 100, -100, 100, 0)
 
-    shape = 't'
-    assert(shape == 't')
+    assert(t == 3*100)
 
-    color_code = 'blue'
-    assert (color_code == 'blue')
+    x = round(turtle.xcor(), 0)
+    assert(x == 100)
 
-    x = 100
-    assert (x == 100)
-
-    y = -100
-    assert (y == -100)
-
-    length = 100
-    assert (length == 100)
-
-    height = 0
-    assert (height == 0)
+    y = round(turtle.ycor(), 0)
+    assert(y == -100)
 
 
